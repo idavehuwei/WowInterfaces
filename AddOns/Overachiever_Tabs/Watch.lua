c6e473bdffa692b57c5fc081082f5f22baf53ca8
wanghong <kuletco@qq.com>
2018-06-09 19:58:38 +0800

    format codea

--- AddOns/Overachiever_Tabs/Watch.lua
+++ AddOns/Overachiever_Tabs/Watch.lua
@@ -1,4 +1,3 @@
-
 --
 --  Overachiever - Tabs: Watch.lua
 --    by Tuhljin
@@ -14,91 +13,91 @@ local frame, panel, sortdrop
 local NoneWatchedLabel
 
 local function SortDrop_OnSelect(self, value)
-  VARS.WatchSort = value
-  frame.sort = value
-  frame:ForceUpdate(true)
+    VARS.WatchSort = value
+    frame.sort = value
+    frame:ForceUpdate(true)
 end
 
 local function OnLoad(v)
-  VARS = v
-  sortdrop:SetSelectedValue(VARS.WatchSort or 0)
-  VARS.WatchedList = VARS.WatchedList or {}
+    VARS = v
+    sortdrop:SetSelectedValue(VARS.WatchSort or 0)
+    VARS.WatchedList = VARS.WatchedList or {}
 end
 
 frame, panel = Overachiever.BuildNewTab("Overachiever_WatchFrame", L.WATCH_TAB,
-                 "Interface\\AddOns\\Overachiever_Tabs\\WatchWatermark", L.WATCH_HELP, OnLoad)
+    "Interface\\AddOns\\Overachiever_Tabs\\WatchWatermark", L.WATCH_HELP, OnLoad)
 
 sortdrop = TjDropDownMenu.CreateDropDown("Overachiever_WatchFrameSortDrop", panel, {
-  {
-    text = L.TAB_SORT_NAME,
-    value = 0
-  },
-  {
-    text = L.TAB_SORT_COMPLETE,
-    value = 1
-  },
-  {
-    text = L.TAB_SORT_POINTS,
-    value = 2
-  },
-  {
-    text = L.TAB_SORT_ID,
-    value = 3
-  };
+    {
+        text = L.TAB_SORT_NAME,
+        value = 0
+    },
+    {
+        text = L.TAB_SORT_COMPLETE,
+        value = 1
+    },
+    {
+        text = L.TAB_SORT_POINTS,
+        value = 2
+    },
+    {
+        text = L.TAB_SORT_ID,
+        value = 3
+    };
 })
 sortdrop:SetLabel(L.TAB_SORT, true)
 sortdrop:SetPoint("TOPLEFT", panel, "TOPLEFT", -16, -22)
 sortdrop:OnSelect(SortDrop_OnSelect)
 
 function frame.SetNumListed(num)
-  if (num > 0) then
-    if (NoneWatchedLabel) then  NoneWatchedLabel:Hide();  end
-  elseif (not NoneWatchedLabel) then
-    NoneWatchedLabel = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
-    NoneWatchedLabel:SetPoint("TOP", frame, "TOP", 0, -189)
-    NoneWatchedLabel:SetText(L.WATCH_EMPTY)
-    NoneWatchedLabel:SetWidth(490)
-  else
-    NoneWatchedLabel:Show()
-  end
+    if (num > 0) then
+        if (NoneWatchedLabel) then NoneWatchedLabel:Hide(); end
+    elseif (not NoneWatchedLabel) then
+        NoneWatchedLabel = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+        NoneWatchedLabel:SetPoint("TOP", frame, "TOP", 0, -189)
+        NoneWatchedLabel:SetText(L.WATCH_EMPTY)
+        NoneWatchedLabel:SetWidth(490)
+    else
+        NoneWatchedLabel:Show()
+    end
 end
 
 
 local function Refresh()
-  local list, count = frame.AchList, 0
-  wipe(list)
-  for id in pairs(VARS.WatchedList) do
-    count = count + 1
-    list[count] = id
-  end
-  Overachiever_WatchFrameContainerScrollBar:SetValue(0)
-  frame:ForceUpdate(true)
+    local list, count = frame.AchList, 0
+    wipe(list)
+    for id in pairs(VARS.WatchedList) do
+        count = count + 1
+        list[count] = id
+    end
+    Overachiever_WatchFrameContainerScrollBar:SetValue(0)
+    frame:ForceUpdate(true)
 end
 
 panel:SetScript("OnShow", Refresh)
 
 function frame.SetAchWatchList(id, add)
-  if (add) then
-    VARS.WatchedList[id] = true
-    PlaySound("igMainMenuOptionCheckBoxOn")
-  else
-    VARS.WatchedList[id] = nil
-    PlaySound("igMainMenuOptionCheckBoxOff")
-  end
-  if (frame:IsShown()) then
-    Refresh()
-  else
-    Overachiever.FlashTab(frame.tab)
-  end
+    if (add) then
+        VARS.WatchedList[id] = true
+        PlaySound("igMainMenuOptionCheckBoxOn")
+    else
+        VARS.WatchedList[id] = nil
+        PlaySound("igMainMenuOptionCheckBoxOff")
+    end
+    if (frame:IsShown()) then
+        Refresh()
+    else
+        Overachiever.FlashTab(frame.tab)
+    end
 end
 
 local orig_AchievementButton_OnClick = AchievementButton_OnClick
 AchievementButton_OnClick = function(self, ignoreModifiers, ...)
-  if (not ignoreModifiers and IsAltKeyDown()) then
-    frame.SetAchWatchList(self.id, true)
-    return;
-  end
-  orig_AchievementButton_OnClick(self, ignoreModifiers, ...)
+    if (not ignoreModifiers and IsAltKeyDown()) then
+        frame.SetAchWatchList(self.id, true)
+        return;
+    end
+    orig_AchievementButton_OnClick(self, ignoreModifiers, ...)
 end
 
 
