14d352644ae8c18f0048c57cc3a522106a71812d
wanghong <kuletco@qq.com>
2018-06-23 10:09:46 +0800

    Update ace3 libs

--- AddOns/Duowan/Acelibs/Ace3/AceConfig-3.0/AceConfig-3.0.lua
+++ AddOns/Duowan/Acelibs/Ace3/AceConfig-3.0/AceConfig-3.0.lua
@@ -3,7 +3,7 @@
 -- as well as associate it with a slash command.
 -- @class file
 -- @name AceConfig-3.0
--- @release $Id: AceConfig-3.0.lua 963 2010-07-26 11:35:35Z mikk $
+-- @release $Id: AceConfig-3.0.lua 969 2010-10-07 02:11:48Z shefki $
 
 --[[
 AceConfig-3.0
@@ -36,7 +36,7 @@ local pcall, error, type, pairs = pcall, error, type, pairs
 -- You can supply a slash command (or a table of slash commands) to register with AceConfigCmd directly.
 -- @paramsig appName, options [, slashcmd]
 -- @param appName The application name for the config table.
--- @param options The option table (or a function to generate one on demand)
+-- @param options The option table (or a function to generate one on demand).  http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/
 -- @param slashcmd A slash command to register for the option table, or a table of slash commands.
 -- @usage
 -- local AceConfig = LibStub("AceConfig-3.0")
