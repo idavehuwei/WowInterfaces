a0145a5f6e96c3b0d8a75c18d3c686fd8753dfc5
WangHong <kuletco@qq.com>
2018-07-26 11:52:55 +0800

    format DBM code

--- AddOns/DBM-ChamberOfAspects/Obsidian/Tenebron.lua
+++ AddOns/DBM-ChamberOfAspects/Obsidian/Tenebron.lua
@@ -1,5 +1,5 @@
-local mod	= DBM:NewMod("Tenebron", "DBM-ChamberOfAspects", 1)
-local L		= mod:GetLocalizedStrings()
+local mod   = DBM:NewMod("Tenebron", "DBM-ChamberOfAspects", 1)
+local L     = mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 3695 $"):sub(12, -3))
 mod:SetCreatureID(30452)
@@ -11,8 +11,8 @@ mod:RegisterEvents(
     "SPELL_CAST_SUCCESS"
 )
 
-local warnShadowFissure	= mod:NewSpellAnnounce(59127)
-local timerShadowFissure = mod:NewCastTimer(5, 59128)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
+local warnShadowFissure = mod:NewSpellAnnounce(59127)
+local timerShadowFissure = mod:NewCastTimer(5, 59128) --Cast timer until Void Blast. it's what happens when shadow fissure explodes.
 
 
 function mod:SPELL_CAST_SUCCESS(args)
